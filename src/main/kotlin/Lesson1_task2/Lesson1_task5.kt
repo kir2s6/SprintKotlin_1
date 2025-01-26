6f41ac94ea458721a380d1b4861a5c086b6aaece
kiril <kirill@>
2025-01-26 21:14:38 +0300

    KS-1-5 refactor task5

--- src/main/kotlin/Lesson1_task2/Lesson1_task5.kt
+++ src/main/kotlin/Lesson1_task2/Lesson1_task5.kt
@@ -6,11 +6,13 @@ package Lesson1_task2
 //- Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
 //- Значения часов, минут и секунд отображать двумя цифрами.
 fun main() {
-    val minutesSpace:Byte = 108
-    val secondsSpace:Short = 6480
-    val hours = "01"
-    val minutes = 30
-    val seconds = "09"
-    println(" Гагарин провел в космосе $hours:$minutes:$seconds")
-    println("Юрий Гагарин провёл в космосе $minutesSpace минут, что составляет $secondsSpace секунд.")
+    val secondsSpace: Short = 6480
+    val hoursSpase = secondsSpace / 3600
+    val remaining = secondsSpace % 3600
+    val minutes = remaining / 60
+    val seconds = remaining % 60
+
+    println(" Гагарин провел в космосе $hoursSpase:$minutes:$seconds")
+    println("Юрий Гагарин провёл в космосе $minutes минут, что составляет $secondsSpace секунд.")
+
 }
